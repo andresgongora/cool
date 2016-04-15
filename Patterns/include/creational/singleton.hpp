c90a6a64e5417f6b28c6eae7e74190627735c300
andresgongora <andresgongora@uma.es>
2016-04-15 14:41:38 +0200

    Added singleton

---
+++ Patterns/include/creational/singleton.hpp
@@ -0,0 +1,81 @@
+// 1 TAB = 8 SPACES // LINE LENGTH = 100 CHARACTERS //
+
+/*	+-----------------------------------------------------------------------+
+	|	          C++ Object Oriented Library (COOL)		        |
+	|		 https://github.com/andresgongora/cool 			|
+	|									|
+	|									|
+	| Copyright (c) 2015 - 2016, Individual contributors, see AUTHORS file.	|
+	| 									|
+	| This program is free software: you can redistribute it and/or modify	|
+	| it under the terms of the GNU General Public License as published by	|
+	| the Free Software Foundation, either version 2 of the License, or	|
+	| (at your option) any later version.					|
+	|									|
+	| This program is distributed in the hope that it will be useful,	|
+	| but WITHOUT ANY WARRANTY; without even the implied warranty of	|
+	| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the		|
+	| GNU General Public License for more details.				|
+	|									|
+	| You should have received a heap of the GNU General Public License	|
+	| along with this program.  If not, see <http://www.gnu.org/licenses/>. |
+	|									|
+	+-----------------------------------------------------------------------+	*/
+
+
+#ifndef __COOL_PATTERN_CREATIONAL_SINGLETON_HPP_INCLUDED__
+#define __COOL_PATTERN_CREATIONAL_SINGLETON_HPP_INCLUDED__
+
+
+namespace cool{ 
+namespace pattern{ 
+namespace creational{
+
+
+
+/***********************************************************************************************//**
+ * @brief	Singleton base class
+ * @warning	All derived classes must befriend with Singleton and use the default private constructor
+ *
+ * @code
+ * 	class myns::MyClass : public Singleton<myns::MyClass>
+ * 	{
+ * 		friend class Singleton<myns::MyClass>;
+ * 	private:
+ * 		MyClass(void) {}
+ *		...
+ *	public:
+ *		...
+ * 	};
+ * @endcode
+ **************************************************************************************************/
+template<typename T_DERIVED>
+class Singleton
+{
+protected:
+				// PROTECTED CONSTRUCTOR
+				Singleton(void)	{}
+
+private:
+				// SINGLETON INSTANCE
+	static T_DERIVED 	instance_;
+
+public:
+				// ACCESSOR
+	static T_DERIVED& 	getInstance(void) 	{ return instance_; }
+	static T_DERIVED& const getConstInstance(void)	{ return instance_; }
+};
+
+
+
+// CREATE INSTANCE
+template<typename T_DERIVED> T_DERIVED Singleton<T_DERIVED>::instance_;
+
+
+
+
+/* ---------------------------------------------------------------------------------------------- */
+}	// namespace cool
+}	// namespace pattern
+}	// namespace creational
+#endif 	// __COOL_PATTERN_CREATIONAL_SINGLETON_HPP_INCLUDED__
